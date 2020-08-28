b2826074b47270c0e720b91dd4a1e1fe60a6fa37
Spurthi Gudigopuram <spurthigudigopuram@gmail.com>
2020-08-28 16:55:15 -0500

    Update knit.R

--- knit.R
+++ knit.R
@@ -1,4 +1,4 @@
-servr:::knit_maybe(input  = c(".", "_source", "_posts"),
+servr:::knit_maybe(input  = c(".", "_posts", "_posts"),
                    output = c(".", "_posts", "_posts"),
                    script = c("build.R", "Makefile"),
                    method = "jekyll")
